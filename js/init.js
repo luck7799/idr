3a33c07b5eb0328fad42dc033940202849a51ad7
luck7799 <luckybianwa@gmail.com>
2016-06-10 11:29:24 +0700

    dsada
    sdasdasdas

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider({full_width: true, height : 328});
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
